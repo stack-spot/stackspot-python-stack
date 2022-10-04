b8452ce1bc7b273341f18906846b1504ea58beba
Francisco Lucas Sens <francisco.sens@zup.com.br>
2022-10-04 18:11:51 -0300

    Adjust method post return status code 201 Created

--- lambda-endpoint-create-plugin/templates/src/{{ resource_folder_name }}/{{ operation_id_sanitized }}/controller.py
+++ lambda-endpoint-create-plugin/templates/src/{{ resource_folder_name }}/{{ operation_id_sanitized }}/controller.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter{% if method == 'POST' %}, status{{"\n"}}{% else %}{{"\n"}}{% endif %}
 from mangum import Mangum
 {% if should_response_as_list %}from typing import List{{"\n"}}{% endif %}
 from src.{{ resource_folder_name }}.{{ operation_id_sanitized }} import usecase
@@ -9,7 +9,7 @@ app = FastAPI()
 router = APIRouter(tags=["{{resource_folder_name}}"])
 
 
-@router.{{ method_sanitized }}("{{ uri }}", response_model={{ resource_response_full_sanitized }})
+@router.{{ method_sanitized }}("{{ uri }}", response_model={{ resource_response_full_sanitized }}{% if method == 'POST' %}, status_code=status.HTTP_201_CREATED{% endif %})
 def {{ operation_id_sanitized }}({% for parameter in parameters %}{{parameter[0]}}: {{parameter[1]}}{% if loop.index + 1 <= loop.length %}{{', '}}{% endif %}{% endfor %}):
     return usecase.{{ operation_id_sanitized }}({% for parameter in parameters %}{{parameter[0]}}{% if loop.index + 1 <= loop.length %}{{', '}}{% endif %}{% endfor %})
 
